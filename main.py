54db0afea9ab3e62d9ab1bc11305c435e4ed487c
aki-abe <akab13@gmail.com>
2020-04-09 15:54:04 +0900

    ローマ字情報の追加

--- main.py
+++ main.py
@@ -8,6 +8,38 @@ import zipfile
 
 def download():
 
+    # 郵便番号データ（ローマ字）のダウンロード
+    response = requests.get("https://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip?190712")
+    if response.status_code != 200:
+        e = Exception(f"HTTP status : {response.status_code}")
+        raise e
+
+    # ダウンロードデータのファイル出力
+    with open("ken_all_rome.zip", "wb") as file:
+        file.write(response.content)
+    
+    # zipファイルの解凍
+    with zipfile.ZipFile("./ken_all_rome.zip") as zip:
+        zip.extractall("./")
+
+    rome_dic = {}
+    rome_file_path = "./KEN_ALL_ROME.CSV"
+    with codecs.open(rome_file_path, "r", encoding="shift-jis") as rome:
+        reader = csv.reader(rome)
+
+        for row in reader:
+            zip_code = row[0]
+            prefecture_name = row[1]
+            city_name = row[2].replace("　", "")
+            town_name = re.sub(r'^(.*?)(（.*)?$', r'\1', row[3]).replace("　", "").replace("以下に掲載がない場合", "")
+            prefecture_rome_name = row[4]
+            city_rome_name = row[5].replace(" ", "-").lower()
+            town_rome_name = re.sub(r'^(.*?)(\(.*)?$', r'\1', row[6]).replace(" ", "").replace("IKANIKEISAIGANAIBAAI", "").lower()
+
+            rome_dic[f"{prefecture_name},{city_name},{town_name},"] = { "prefecture_name": prefecture_rome_name, "city_name": city_rome_name, "town_name": town_rome_name }
+
+
+
     # 郵便番号データのダウンロード
     response = requests.get("https://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip")
     if response.status_code != 200:
@@ -37,6 +69,8 @@ def download():
         town_name = ""
         town_short_name = ""
         town_ext_name = ""
+        city_rome_name = ""
+        town_rome_name = ""
         town_duplicate_flag = ""
         town_multi_flag = ""
         zip_code_branch_no = {}
@@ -49,14 +83,12 @@ def download():
 
 
         for row in reader:
-            if zip_code == row[2] and ("（" in town_name) and not re.match(r".*）$", town_name):
+            if zip_code.replace("-", "") == row[2] and ("（" in town_name) and (not town_name.endswith("）")):
                 same_zip_code = True
             else:
                 same_zip_code = False
 
             if not same_zip_code and zip_code:
-                town_name.replace("−", "-").replace("〜", "～")
-                town_kana_name = mojimoji.han_to_zen(town_kana_name)
 
                 if town_name == "以下に掲載がない場合":
                     town_short_name = ""
@@ -91,7 +123,15 @@ def download():
                 else:
                     zip_code_branch_no[zip_code] = 1
 
-                dest.write(f"{zip_code},{zip_code_branch_no[zip_code]},{area_code},{prefecture_name},{city_name},{town_short_name},{town_ext_name},{mojimoji.han_to_zen(city_kana_name)},{town_short_kana_name},{town_ext_kana_name},{town_duplicate_flag},{building_flag}\n")
+                key = f"{prefecture_name},{city_name},{town_short_name},"
+                if key in rome_dic:
+                    city_rome_name = rome_dic[key]["city_name"]
+                    town_rome_name = rome_dic[key]["town_name"]
+                else:
+                    city_rome_name = ""
+                    town_rome_name = ""
+
+                dest.write(f"{zip_code},{zip_code_branch_no[zip_code]},{area_code},{prefecture_name},{city_name},{town_short_name},{town_ext_name},{mojimoji.han_to_zen(city_kana_name)},{town_short_kana_name},{town_ext_kana_name},{city_rome_name},{town_rome_name},{town_duplicate_flag},{building_flag}\n")
 
             area_code = row[0] # 全国地方公共団体コード
             # xxx = row[1] # 旧郵便番号5桁
@@ -99,9 +139,11 @@ def download():
             # xxx = row[3] # 都道府県名（半角カタカナ）
             city_kana_name = row[4] # 市区町村名（半角カタカナ）
             town_kana_name = row[5] if not same_zip_code else f"{town_kana_name}{row[5]}" # 町域名（半角カタカナ）
+            town_kana_name = mojimoji.han_to_zen(town_kana_name)
             prefecture_name = row[6] # 都道府県名
             city_name = row[7] # 市区町村名
             town_name = row[8] if not same_zip_code else f"{town_name}{row[8]}" # 町域
+            town_name = town_name.replace("−", "-").replace("〜", "～")
             town_duplicate_flag = row[9] # 一町域が二以上の郵便番号で表される場合の表示　（注3）　（「1」は該当、「0」は該当せず）
             # xxx = row[10] # 小字毎に番地が起番されている町域の表示　（注4）　（「1」は該当、「0」は該当せず）
             # xxx = row[11] # 丁目を有する町域の場合の表示　（「1」は該当、「0」は該当せず）
@@ -109,8 +151,6 @@ def download():
             # xxx = row[13] # 更新の表示（注6）（「0」は変更なし、「1」は変更あり、「2」廃止（廃止データのみ使用））
             # xxx = row[14] # 変更理由　（「0」は変更なし、「1」市政・区政・町政・分区・政令指定都市施行、「2」住居表示の実施、「3」区画整理、「4」郵便区調整等、「5」訂正、「6」廃止（廃止データのみ使用））
 
-
-        town_kana_name = mojimoji.han_to_zen(town_kana_name)
 
         town_short_name = re.sub(r"（.*）", "", town_name)
         town_ext_name = town_name.replace(town_short_name, "", 1)
@@ -133,7 +173,15 @@ def download():
         else:
             zip_code_branch_no[zip_code] = 1
 
-        dest.write(f"{zip_code},{zip_code_branch_no[zip_code]},{area_code},{prefecture_name},{city_name},{town_short_name},{town_ext_name},{mojimoji.han_to_zen(city_kana_name)},{town_short_kana_name},{town_ext_kana_name},{town_duplicate_flag},{building_flag}\n")
+        key = f"{prefecture_name},{city_name},{town_short_name},"
+        if key in rome_dic:
+            city_rome_name = rome_dic[key]["city_name"]
+            town_rome_name = rome_dic[key]["town_name"]
+        else:
+            city_rome_name = ""
+            town_rome_name = ""
+
+        dest.write(f"{zip_code},{zip_code_branch_no[zip_code]},{area_code},{prefecture_name},{city_name},{town_short_name},{town_ext_name},{mojimoji.han_to_zen(city_kana_name)},{town_short_kana_name},{town_ext_kana_name},{city_rome_name},{town_rome_name},{town_duplicate_flag},{building_flag}\n")
 
 
 if __name__ == "__main__":
